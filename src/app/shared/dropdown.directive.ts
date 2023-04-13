02d0daadd3f938c92db8b12532746cbae9e7a1fc
Nishant Sanjay Kumbhar <nishantsk210@gmail.com>
2023-04-13 09:22:35 +0530

    Section 8 - Direectives

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen : boolean = false;
+  constructor() { }
+
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
+}
